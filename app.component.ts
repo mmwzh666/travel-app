22221c53e1a07a5a5a147124b7013ea44b1c7bea
mmwzh666 <50340848+mmwzh666@users.noreply.github.com>
2020-10-12 19:56:49 -0500

    和我

--- app.component.ts
+++ app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Zeheng Wu';
+  title = "Zeheng Wu's Travel Cost Calculator";
+
+
+  oriAirport: string = 'Your Origin';
+  desAirport: string = 'Your destination';
+  date: Date;
+  travelers: number = 0;
+  cost: number = 0;
+
+  travelCost(): void {
+    this.cost = this.travelers * 150;
+  }
 }
