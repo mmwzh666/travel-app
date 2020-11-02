5bae94ebe8f9eec3bf244acb6f910446b487831a
mmwzh666 <50340848+mmwzh666@users.noreply.github.com>
2020-11-02 00:53:14 -0600

    Add New Content for HW3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
+import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, Component } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import {TravelRequestComponent} from './travel_request/travelRequest.component';
+import {AboutComponent} from './about/about.component';
+
+const routes: Routes = [
+  {path: 'travel_request', component: TravelRequestComponent},
+  {path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TravelRequestComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
